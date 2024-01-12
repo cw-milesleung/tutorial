6d8cec75114aa015bd1989abba913ccc50f58d55
miles25 <miles@databased.com>
2024-01-12 14:25:00 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Welcome!</h2>
+      <h2>Welcome Vite + React!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
